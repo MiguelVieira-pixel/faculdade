f678b5b2e8e600967b3f1c690e88ee0558b921d1
Miguel Vieira <gitprogama@gmail.com>
2026-02-12 22:28:11 -0300

    aula1_site_dinamico

---
+++ estrutura_de_dados/strings.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    int x ;
+    char nome[30] = "Nikola Tesla";
+    printf("\n Um inteiro ocupa %ld bytes\n", sizeof(x))
+    printf("\n Um inteiro ocupa %ld bytes\n", sizeof(x))
+    printf("\n Nome = [%s]\n", nome);
+    nome[6] = '\n';
+    printf("\n Nome = [%s]\n", nome);
+}
