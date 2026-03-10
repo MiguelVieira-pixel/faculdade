269526f9d5284809165b01e97526fd3d7a17c514
Miguel Vieira <gitprogama@gmail.com>
2026-03-10 19:18:45 -0300

    Refactor busca_sequencial.c: update Estado struct, improve buscaEstado function, and enhance mostraEstado output Add new executables for busca_sequencial and teste_boolean Create teste_boolean.c for boolean testing example

--- estrutura_de_dados/busca_sequencial.c
+++ estrutura_de_dados/busca_sequencial.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 // este program mostra como realizar
 // uma busca linear em um vertor de estruturas
 // completar em casa
 
 typedef struct{
-    char codigo[2];
+    char codigo[3];
     char extenso[40];
     int populacao;
     float rpc; //renda per capita
@@ -15,26 +17,40 @@ Estado e[] = {
     {"AL", "ALAGOAS", 57, 11.00},
     {"MT", "MATO GROSSO", 58, 12.00},
     {"SP", "SAO PAULO", 59, 13.00},
-    {"SP", "SAO PAULO", 59, 13.00},
-    {"TO", "TOCATINS", 60, 14.00}
+    {"MG", "MINAS GERAIS", 60, 14.00},
+    {"TO", "TOCATINS", 61, 15.00}
 };
 
-void mostraEstado( Estado e){
-    printf("\n     %s Pop=%d habitantes RPC=%d\n\n", e.codigo, e.extenso, e.populacao, e.rpc);
+void mostraEstado( Estado pe){
+    printf("\n     %s | Pop = %d habitantes | RPC= R$ %.2f\n\n", pe.codigo, pe.extenso, pe.populacao, pe.rpc);
 }
 
-// retorna a posição do vetor
-int buscaEstado(char siglaProcurada[3]){
-    if(buscaEstado() == )
+// retorna a posicao do vetor ou -1 se n�o encontrar
+int buscaEstado(char sp[3]){	//Sigla procurada = sp
+    int pos = 0;	//posicao no vetor
+    int retorno, i = 0;
+	int qtdEstado =  sizeof(e) / sizeof(Estado);
+	
+	for (i = 0; sp[i]; i++) { //Converte as letras em maiuscula
+        sp[i] = toupper(sp[i]);
+    }
+	
+	while(pos < qtdEstado && strcmp(sp, e[pos].codigo))
+		pos++;
+	if(pos >= qtdEstado)
+		retorno = -1;
+	else
+		retorno = pos;
+	return retorno;
 }
 
 void main(){
     char desejado[3];
-    int posicao = buscaEstado(desejado);
     printf("Informe a sigla do estado desejado: ");
     scanf("%s", desejado);
+    int posicao = buscaEstado(desejado);
     if(posicao == -1)
-        printf("\nSigla não encontrada\n");
+        printf("\nSigla nao encontrada\n");
     else
         mostraEstado(e[posicao]);
 }
---
+++ estrutura_de_dados/teste_boolean.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+main(){
+	int teste = 6;
+	if(teste)
+		printf("\n Verdadeiro\n");
+	else
+		printf("\nFalso");
+}
